9cc34d9579389e088dbdf1c9fb5e09880ece10cf
muneebkhan4 <muneebkhan4@outlook.com>
2023-07-08 21:32:28 +0500

    Deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    }).compileComponents()
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +16,72 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tic-tac-toe'`, () => {
+  it(`should have 'X' as the initial current player`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tic-tac-toe');
+    expect(app.currentPlayer).toEqual('X');
   });
 
-  it('should render title', () => {
+  it(`should display 'Current Player: X' on the board`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe app is running!');
+    const titleElement = compiled.querySelector('h2');
+    expect(titleElement).not.toBeNull();
+    expect(titleElement?.textContent).toContain('Current Player: X');
+  });
+
+  it(`should switch current player to 'O' after making a move`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.makeMove(0, 0); // X makes a move
+    expect(app.currentPlayer).toEqual('O');
+  });
+
+  it(`should display 'Current Player: O' after making a move`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.makeMove(0, 0); // X makes a move
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titleElement = compiled.querySelector('h2');
+    expect(titleElement).not.toBeNull();
+    expect(titleElement?.textContent).toContain('Current Player: O');
+  });
+
+  it(`should display 'Player  X  Wins!' when X wins the game`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    // Simulate winning moves for X
+    app.makeMove(0, 0); // X
+    app.makeMove(1, 0); // O
+    app.makeMove(0, 1); // X
+    app.makeMove(1, 1); // O
+    app.makeMove(0, 2); // X
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titleElement = compiled.querySelector('h2');
+    expect(titleElement).not.toBeNull();
+    expect(titleElement?.textContent).toContain('Player  X  Wins!');
+  });
+
+  it(`should display "It's a Tie!" when the game ends in a tie`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    // Simulate moves to end in a tie
+    app.makeMove(0, 0); // X
+    app.makeMove(0, 1); // O
+    app.makeMove(0, 2); // X
+    app.makeMove(1, 0); // O
+    app.makeMove(1, 2); // X
+    app.makeMove(1, 1); // O
+    app.makeMove(2, 0); // X
+    app.makeMove(2, 2); // O
+    app.makeMove(2, 1); // X
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titleElement = compiled.querySelector('h2');
+    expect(titleElement).not.toBeNull();
+    expect(titleElement?.textContent).toContain("It's a Tie!");
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,127 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+  board: string[][];
+  currentPlayer: string;
+  player1: string;
+  player2: string;
+  gameOver: boolean;
+  winner: string;
+
+  constructor() {
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+    this.currentPlayer = 'X';
+    this.player1 = 'X';
+    this.player2 = 'O';
+    this.gameOver = false;
+    this.winner = '';
+  }
+
+  makeMove(row: number, col: number) {
+    if (!this.gameOver && this.board[row][col] === '') {
+      this.board[row][col] = this.currentPlayer;
+      this.checkWinner(row, col);
+      this.currentPlayer =
+        this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    }
+  }
+
+  checkWinner(row: number, col: number) {
+    const symbol = this.board[row][col];
+    let won = true;
+
+    // Check row
+    for (let c = 0; c < 3; c++) {
+      if (this.board[row][c] !== symbol) {
+        won = false;
+        break;
+      }
+    }
+
+    if (won) {
+      this.gameOver = true;
+      this.winner = symbol;
+      return;
+    }
+
+    won = true;
+
+    // Check column
+    for (let r = 0; r < 3; r++) {
+      if (this.board[r][col] !== symbol) {
+        won = false;
+        break;
+      }
+    }
+
+    if (won) {
+      this.gameOver = true;
+      this.winner = symbol;
+      return;
+    }
+
+    // Check diagonal
+    if (row === col) {
+      won = true;
+      for (let i = 0; i < 3; i++) {
+        if (this.board[i][i] !== symbol) {
+          won = false;
+          break;
+        }
+      }
+      if (won) {
+        this.gameOver = true;
+        this.winner = symbol;
+        return;
+      }
+    }
+
+    // Check reverse diagonal
+    if (row + col === 2) {
+      won = true;
+      for (let i = 0; i < 3; i++) {
+        if (this.board[i][2 - i] !== symbol) {
+          won = false;
+          break;
+        }
+      }
+      if (won) {
+        this.gameOver = true;
+        this.winner = symbol;
+        return;
+      }
+    }
+
+    // Check tie
+    let tie = true;
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
+        if (this.board[r][c] === '') {
+          tie = false;
+          break;
+        }
+      }
+    }
+    if (tie) {
+      this.gameOver = true;
+    }
+  }
+
+  resetGame() {
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+    this.currentPlayer = this.player1;
+    this.gameOver = false;
+    this.winner = '';
+  }
 }
